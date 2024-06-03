65dce6e87d62dce70a8a57bfe7c05cab72cfb8a4
Philbert <philbertshea@gmail.com>
2024-06-03 15:26:42 +0800

    First Commit

---
+++ resale.py
@@ -0,0 +1,130 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, StaleElementReferenceException
+import xlsxwriter
+import time
+
+driver = webdriver.Chrome()
+ROOT = "https://homes.hdb.gov.sg/home/finding-a-flat"
+
+def scrape_page(driver, start_page, worksheet):
+    driver.get(ROOT)
+    time.sleep(3)                                               
+    section = driver.find_element(By.CSS_SELECTOR, "body").find_element(By.CSS_SELECTOR, "div[class='listing-portion']")
+    section = section.find_element(By.CSS_SELECTOR, "div[class='listings']").find_element(By.CSS_SELECTOR, "div[class='container']")
+    section.find_element(By.XPATH, ".//div[4]/app-flat-cards-categories").click()
+    time.sleep(3)
+
+    worksheet.write(0, 0, "Address")
+    worksheet.write(0, 1, "Address_2")
+    worksheet.write(0, 2, "Price")
+    worksheet.write(0, 3, "Flat Type")
+    worksheet.write(0, 4, "Flat Area")
+    worksheet.write(0, 5, "Town")
+    worksheet.write(0, 6, "Storey")
+    worksheet.write(0, 7, "Remaining Lease")
+    worksheet.write(0, 8, "Range: Low")
+    worksheet.write(0, 9, "Range: High")
+    worksheet.write(0, 10, "Price relative to Range: High")
+    worksheet.write(0, 11, "Link to property")
+    count = (start_page - 1) * 20 + 1
+    next_exists = True
+    
+    for i in range(1, start_page, 1):
+        try:
+        # Click "Right" button
+            section = driver.find_element(By.CSS_SELECTOR, "body").find_element(By.CSS_SELECTOR, "div[class='listing-portion']")
+            section = section.find_element(By.CSS_SELECTOR, "div[class='listings']").find_element(By.CSS_SELECTOR, "div[class='container']")
+                
+            section.find_element(By.XPATH, ".//div[5]/div/nav/ngb-pagination/ul/li[8]").click()
+        except Exception as e:
+            print(e)
+            break
+
+    while next_exists:
+        try:
+            for i in range(1, 21, 1):
+                print(f"get {count} - {i}")
+                # driver.refresh()
+                section = driver.find_element(By.CSS_SELECTOR, "body").find_element(By.CSS_SELECTOR, "div[class='listing-portion']")
+                section = section.find_element(By.CSS_SELECTOR, "div[class='listings']").find_element(By.CSS_SELECTOR, "div[class='container']")
+                item = section.find_element(By.XPATH, f".//div[4]/app-flat-cards[{i}]")
+                item.click()
+                time.sleep(2)
+                try:
+                    scrape_individual(driver, count, worksheet)
+                except Exception as e:
+                    print(e)
+                    print(f"Skipped {count} - {i}")
+                driver.execute_script("window.history.go(-1)")
+                time.sleep(2)
+                count = count + 1
+                
+
+        # Click "Right" button
+            section = driver.find_element(By.CSS_SELECTOR, "body").find_element(By.CSS_SELECTOR, "div[class='listing-portion']")
+            section = section.find_element(By.CSS_SELECTOR, "div[class='listings']").find_element(By.CSS_SELECTOR, "div[class='container']")
+                
+            section.find_element(By.XPATH, ".//div[5]/div/nav/ngb-pagination/ul/li[8]").click()
+        except Exception as e:
+            print(e)
+            break
+
+def scrape_individual(driver, count, worksheet):
+    section = driver.find_element(By.CSS_SELECTOR, "app-resale-flat-details").find_element(By.CSS_SELECTOR, "div[class='flat-details']")
+    top_block = section.find_element(By.CSS_SELECTOR, "div[class='border-bottom']")
+    add_price = top_block.find_element(By.CSS_SELECTOR, "app-overview").find_element(By.XPATH, ".//div[1]")
+    add_block = add_price.find_element(By.XPATH, ".//div[1]")
+    worksheet.write(count, 11, driver.current_url)
+    address = add_block.find_element(By.CSS_SELECTOR, "h3").text
+    worksheet.write(count, 0, address)
+    address_2 = add_block.find_element(By.CSS_SELECTOR, "h5").text
+    worksheet.write(count, 1, address_2)
+    details = add_block.find_element(By.CSS_SELECTOR, "p").text.splitlines()
+    flat_type = details[0]
+    flat_area = details[1]
+    worksheet.write(count, 3, flat_type)
+    worksheet.write(count, 4, flat_area)
+
+    # /div[@class='flat-details']/div[@class='border-bottom']/app-overview/div[1]
+    price_block = add_price.find_element(By.XPATH, ".//div[2]/div/div")
+    price = int(price_block.find_element(By.CSS_SELECTOR, "h2").text[1:].replace(',', ''))
+    worksheet.write(count, 2, price)
+
+    bottom_block = section.find_element(By.CSS_SELECTOR, "div[class='container my-10']")
+    info_block = bottom_block.find_element(By.XPATH, ".//app-key-details/div/div")
+    town = info_block.find_element(By.XPATH, ".//div/div[4]").text.splitlines()[1]
+    storey = info_block.find_element(By.XPATH, ".//div/div[5]").text.splitlines()[1]
+    rem_lease = info_block.find_element(By.XPATH, ".//div/div[6]").text.splitlines()[1]
+    worksheet.write(count, 5, town)
+    worksheet.write(count, 6, storey)
+    worksheet.write(count, 7, rem_lease)
+    price_range = info_block.find_element(By.XPATH, ".//div[3]/div[3]").text.splitlines()[1]
+    if price_range.startswith("No"):
+        worksheet.write(count, 8, "NA")
+        worksheet.write(count, 9, "NA")
+        worksheet.write(count, 10, "NA")
+    else:
+        floor = int(price_range.split(' ')[0][1:].replace(',', ''))
+        ceil = int(price_range.split(' ')[2][1:].replace(',', ''))
+        worksheet.write(count, 8, floor)
+        worksheet.write(count, 9, ceil)
+        percentile = (price / ceil) * 100
+        worksheet.write(count, 10, percentile)
+    
+def get_all_resale_data(start_page):
+    workbook = xlsxwriter.Workbook('resaledata.xlsx')
+    worksheet = workbook.add_worksheet()
+    try:
+        scrape_page(driver, start_page, worksheet)
+    finally:
+        workbook.close()
+
+# Start should be 20*k + 1, where k is an integer
+get_all_resale_data(8)

37247e56ea1e500a1c8071d74e876ed53dfb2556
Philbert <philbertshea@gmail.com>
2024-06-05 12:26:06 +0800

    Added more data columns

--- resale.py
+++ resale.py
@@ -12,6 +12,7 @@ import time
 
 driver = webdriver.Chrome()
 ROOT = "https://homes.hdb.gov.sg/home/finding-a-flat"
+ROOT2 = "https://"
 
 def scrape_page(driver, start_page, worksheet):
     driver.get(ROOT)
@@ -26,13 +27,22 @@ def scrape_page(driver, start_page, worksheet):
     worksheet.write(0, 2, "Price")
     worksheet.write(0, 3, "Flat Type")
     worksheet.write(0, 4, "Flat Area")
-    worksheet.write(0, 5, "Town")
-    worksheet.write(0, 6, "Storey")
-    worksheet.write(0, 7, "Remaining Lease")
-    worksheet.write(0, 8, "Range: Low")
-    worksheet.write(0, 9, "Range: High")
-    worksheet.write(0, 10, "Price relative to Range: High")
-    worksheet.write(0, 11, "Link to property")
+    worksheet.write(0, 5, "Recent Low")
+    worksheet.write(0, 6, "Recent High")
+    worksheet.write(0, 7, "Price relative to Recent High")
+    worksheet.write(0, 8, "Town")
+    worksheet.write(0, 9, "Storey")
+    worksheet.write(0, 10, "Remaining Lease")
+    worksheet.write(0, 11, "Number of bathrooms")
+    worksheet.write(0, 12, "Number of bedrooms")
+    worksheet.write(0, 13, "Balcony")
+    worksheet.write(0, 14, "Contra")
+    worksheet.write(0, 15, "Extension of stay")
+    worksheet.write(0, 16, "Upgrading")
+    worksheet.write(0, 17, "Ethnic Eligibility")
+    worksheet.write(0, 18, "SPR Eligibility")
+    worksheet.write(0, 19, "Agent Listing?")
+    worksheet.write(0, 20, "Link to property")
     count = (start_page - 1) * 20 + 1
     next_exists = True
     
@@ -81,7 +91,7 @@ def scrape_individual(driver, count, worksheet):
     top_block = section.find_element(By.CSS_SELECTOR, "div[class='border-bottom']")
     add_price = top_block.find_element(By.CSS_SELECTOR, "app-overview").find_element(By.XPATH, ".//div[1]")
     add_block = add_price.find_element(By.XPATH, ".//div[1]")
-    worksheet.write(count, 11, driver.current_url)
+    worksheet.write(count, 20, driver.current_url)
     address = add_block.find_element(By.CSS_SELECTOR, "h3").text
     worksheet.write(count, 0, address)
     address_2 = add_block.find_element(By.CSS_SELECTOR, "h5").text
@@ -102,21 +112,46 @@ def scrape_individual(driver, count, worksheet):
     town = info_block.find_element(By.XPATH, ".//div/div[4]").text.splitlines()[1]
     storey = info_block.find_element(By.XPATH, ".//div/div[5]").text.splitlines()[1]
     rem_lease = info_block.find_element(By.XPATH, ".//div/div[6]").text.splitlines()[1]
-    worksheet.write(count, 5, town)
-    worksheet.write(count, 6, storey)
-    worksheet.write(count, 7, rem_lease)
+    bathroom = info_block.find_element(By.XPATH, ".//div/div[7]").text.splitlines()[1]
+    bedroom = info_block.find_element(By.XPATH, ".//div/div[8]").text.splitlines()[1]
+    balcony = info_block.find_element(By.XPATH, ".//div/div[9]").text.splitlines()[1]
+    contra = info_block.find_element(By.XPATH, ".//div/div[10]").text.splitlines()[1]
+    extension = info_block.find_element(By.XPATH, ".//div/div[11]").text.splitlines()[1]
+    upgrading = info_block.find_element(By.XPATH, ".//div/div[12]").text.splitlines()[1]
+    ethnic = info_block.find_element(By.XPATH, ".//div/div[13]").text.splitlines()[1]
+    spr = info_block.find_element(By.XPATH, ".//div/div[14]").text.splitlines()[1]
+    worksheet.write(count, 8, town)
+    worksheet.write(count, 9, storey)
+    worksheet.write(count, 10, rem_lease)
+    worksheet.write(count, 11, bathroom)
+    worksheet.write(count, 12, bedroom)
+    worksheet.write(count, 13, balcony)
+    worksheet.write(count, 14, contra)
+    worksheet.write(count, 15, extension)
+    worksheet.write(count, 16, upgrading)
+    worksheet.write(count, 17, ethnic)
+    worksheet.write(count, 18, spr)
     price_range = info_block.find_element(By.XPATH, ".//div[3]/div[3]").text.splitlines()[1]
     if price_range.startswith("No"):
-        worksheet.write(count, 8, "NA")
-        worksheet.write(count, 9, "NA")
-        worksheet.write(count, 10, "NA")
+        worksheet.write(count, 5, "N/A")
+        worksheet.write(count, 6, "N/A")
+        worksheet.write(count, 7, "0")
     else:
         floor = int(price_range.split(' ')[0][1:].replace(',', ''))
         ceil = int(price_range.split(' ')[2][1:].replace(',', ''))
-        worksheet.write(count, 8, floor)
-        worksheet.write(count, 9, ceil)
+        worksheet.write(count, 5, floor)
+        worksheet.write(count, 6, ceil)
         percentile = (price / ceil) * 100
-        worksheet.write(count, 10, percentile)
+        worksheet.write(count, 7, percentile)
+    
+    description_block = info_block.find_element(By.XPATH, ".//div[2]/div/div[2]/div/div/div/div")
+    description_block.find_element(By.XPATH, ".//div[2]").click()
+
+    agent_text = description_block.find_element(By.XPATH, ".//div/div").text
+    if 'CEA' in agent_text:
+        worksheet.write(count, 19, "Agent")
+    else:
+        worksheet.write(count, 19, "Non-Agent")
     
 def get_all_resale_data(start_page):
     workbook = xlsxwriter.Workbook('resaledata.xlsx')
@@ -125,6 +160,13 @@ def get_all_resale_data(start_page):
         scrape_page(driver, start_page, worksheet)
     finally:
         workbook.close()
+    driver.close()
+
+def add_to_mymaps(driver):
+    driver.get(ROOT2)
+    time.sleep(2)
+    driver.find_element(By.CSS_SELECTOR, "div[id='featurelist-titlebar-container']")
+    return
 
 # Start should be 20*k + 1, where k is an integer
-get_all_resale_data(8)
+get_all_resale_data(33)
